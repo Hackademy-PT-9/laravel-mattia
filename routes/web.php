2bc9ef58486c6f1c1e377788abaeb2e6ce451164
Mattia-Carcione <mattia.carcione01@gmail.com>
2023-08-11 23:20:06 +0200

    aggiornamento routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/homepage', function () {
     return view('welcome');
 })->name('homepage');
