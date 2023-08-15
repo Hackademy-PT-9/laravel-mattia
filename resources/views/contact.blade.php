d58b99a1120ba04b74dd75b7a41d55384ed4890b
Mattia-Carcione <mattia.carcione01@gmail.com>
2023-08-15 08:41:14 +0200

    add css

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -14,17 +14,17 @@
         <div
             class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
 
+            <div class="col-md-3 text-end">
+                <ul class="nav col-12 col-md-auto mb-2 justify-content-start mb-md-0">
+                    <li><a href="{{ route('homepage') }}" class="nav-link px-2 link-secondary">Home</a></li>
+                </ul>
+            </div>
+
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="{{ route('homepage') }}" class="nav-link px-2 link-secondary">Home</a></li>
                 <li><a href="{{ route('about') }}" class="nav-link px-2">Chi sono</a></li>
                 <li><a href="{{ route('services') }}" class="nav-link px-2">Servizi</a></li>
                 <li><a href="{{ route('contact') }}" class="nav-link px-2">Contatti</a></li>
             </ul>
-
-            <div class="col-md-3 text-end">
-                <button type="button" class="btn btn-outline-primary me-2">Login</button>
-                <button type="button" class="btn btn-primary">Sign-up</button>
-            </div>
 
         </div>
     </nav>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,6 +7,7 @@
     <title>{{ env('APP_NAME') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        <link rel="stylesheet" href="\style.css">
 </head>
 
 <body>
@@ -14,20 +15,28 @@
         <div
             class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
 
+            <div class="col-md-3 text-end">
+                <ul class="nav col-12 col-md-auto mb-2 justify-content-start mb-md-0">
+                    <li><a href="{{ route('homepage') }}" class="nav-link px-2 link-secondary">Home</a></li>
+                </ul>
+            </div>
+
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="{{ route('homepage') }}" class="nav-link px-2 link-secondary">Home</a></li>
                 <li><a href="{{ route('about') }}" class="nav-link px-2">Chi sono</a></li>
                 <li><a href="{{ route('services') }}" class="nav-link px-2">Servizi</a></li>
                 <li><a href="{{ route('contact') }}" class="nav-link px-2">Contatti</a></li>
             </ul>
 
-            <div class="col-md-3 text-end">
-                <button type="button" class="btn btn-outline-primary me-2">Login</button>
-                <button type="button" class="btn btn-primary">Sign-up</button>
-            </div>
-
         </div>
     </nav>
+    <main class="container" style="margin-top: 70px;">
+        <header>
+
+        </header>
+        <footer>
+
+        </footer>
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
     </script>
