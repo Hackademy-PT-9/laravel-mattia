508aacd4498ea0ed337298e977f4d76fc7eb8fc7
Mattia-Carcione <mattia.carcione01@gmail.com>
2023-08-24 19:19:41 +0200

    aggiustato link css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,7 +22,7 @@
 <!-- Core theme CSS (includes Bootstrap)-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-<link href="css/style.css" rel="stylesheet" />
+<link href="/css/style.css" rel="stylesheet" />
 </head>
 
 <body>
