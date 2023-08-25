02bcf1e9049b11118c91fdba332aae4fdc318d1b
Mattia-Carcione <mattia.carcione01@gmail.com>
2023-08-25 18:14:31 +0200

    add nav css

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -2,8 +2,10 @@
     <div class="card" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title text-center">{{ $detail['name'] }}</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text">Descrizione: <br>
+                per <strong>{{ $detail['name'] }}</strong> il tempo di realizzazione è di circa
+                <u>{{ $detail['tempo_di_realizzazione'] }}</u>.
+                <br>Il prezzo è di {{ $detail['costo'] }}€</p>
             <div class="d-flex justify-content-center">
                 <a href="{{ route('detail', ['detail' => $detail['uri']]) }}" class="btn btn-primary">Get Started</a>
             </div>
